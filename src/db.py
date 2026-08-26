56a2e3e45c962e8f983a77a443fae7fe654b3ca3
Abhigya Narain <narainabhigya27@gmail.com>
2026-08-26 21:36:33 +0530

    database file

--- src/db.py
+++ src/db.py
@@ -2,25 +2,31 @@ from collections.abc import AsyncGenerator
 from datetime import datetime
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, DateTime, String, Text
+from sqlalchemy import Column, DateTime, String, Text, ForeignKey
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
+from sqlalchemy.orm import DeclarativeBase, relationship
+from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
+from fastapi import Depends
+
 import os
 load_dotenv()
 
-
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL)
 
 session_maker = async_sessionmaker(bind=engine)
 Base = declarative_base()
 
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    posts = relationship("Post", back_populates="user")
 
 class Post(Base):
     __tablename__ = "posts"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
     title = Column(Text)
     url = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
@@ -36,3 +42,6 @@ async def create_db_model():
 async def get_session():
     async with session_maker() as session:
         yield session
+
+async def get_user_db(session: AsyncSession = Depends(get_session)):
+    yield SQLAlchemyUserDatabase(session, User)
